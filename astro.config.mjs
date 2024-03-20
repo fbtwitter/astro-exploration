b92cde35f8bc8fad06a6370a742f1c0c1414b563
Reza Fauzi Augusdi <rzaugusdi354@gmail.com>
2024-03-20 14:17:23 +0700

    feat: site ready to deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,20 @@
 import { defineConfig } from 'astro/config';
-
 import react from "@astrojs/react";
 import solidJs from "@astrojs/solid-js";
 
+import netlify from "@astrojs/netlify/functions";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [
-    react({
-      include: ['**/react/*'],
-    }),
-    solidJs({
-      include: ['**/solid/*', '**/node_modules/@suid/material/**'],
-    })
-  ]
+  site: "https://astro-frontend-masters.netlify.app",
+  output: "hybrid",
+  experimental: {
+    hybridOutput: true
+  },
+  integrations: [react({
+    include: ['**/react/*']
+  }), solidJs({
+    include: ['**/solid/*', '**/node_modules/@suid/material/**']
+  })],
+  adapter: netlify()
 });
